3c061a631b2d992c8a2ff3ef0645fa10c514cea6
ihor-seven <kryskoihor@gmail.com>
2026-03-02 16:12:07 +0200

    Solutin 2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,6 +1,12 @@
 from app.split_integer import split_integer
 
 
+def test_difference_between_max_and_min_should_not_exceed_one() -> None:
+    assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
+    assert max(split_integer(32, 6)) - min(split_integer(32, 6)) <= 1
+    assert max(split_integer(5, 2)) - min(split_integer(5, 2)) <= 1
+
+
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(8, 1)) == 8
     assert sum(split_integer(6, 2)) == 6
@@ -22,12 +28,9 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
-    result = split_integer(32, 6)
-    assert result == sorted(result)
-    result = split_integer(5, 2)
-    assert result == sorted(result)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(5, 2) == [2, 3]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
